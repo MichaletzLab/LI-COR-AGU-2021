a13fabd2361234fc0b663f7a960bae64e4ac0c77
Josef Garen <44302138+garenj@users.noreply.github.com>
2022-06-12 09:59:35 -0700

    Update example_analysis.R
    Added comment block explaining machine-specific nature of corrections

--- scripts/example_analysis.R
+++ scripts/example_analysis.R
@@ -2,6 +2,20 @@
 # Garen et al. 2021
 # AGU 2021
 # example_analysis.R
+#
+# This file illustrates the use of the correct_licor6400 function defined
+# in the associated file correct_licor6400.R.
+#
+# The parameter values used in the correction functions defined in the associated
+# file correct_lifor6400.R are likely to vary in a manner that is specific 
+# to an individual instrument and set of environmental conditions. We urge
+# potential users of this code to independently measure leaf and air temperatures
+# in their LI-COR instruments and environmental conditions to parameterize these
+# correction functions prior to use.
+#
+# For more details see associated forthcoming article, Garen et al., New
+# Phytologist (in press).
+
 
 # Install LICOR6400 package for helper functions if necessary
 if(!require(LICOR6400)){
@@ -32,7 +46,3 @@ rm("licor_uncorrected", "licor_averaged")
 
 # Perform analyses of interest with corrected data
 plot(licor_corrected$Tleaf, licor_corrected$Photo)
-
-
-
-
